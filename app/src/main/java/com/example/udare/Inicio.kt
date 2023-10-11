8635d910e2d80a27051c8c1b2da71c641d49ee6f
javi <javi.polit@hotmail.com>
2023-10-11 16:25:15 +0200

    .

--- app/src/main/java/com/example/udare/Inicio.kt
+++ app/src/main/java/com/example/udare/Inicio.kt
@@ -2,10 +2,41 @@ package com.example.udare
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.graphics.drawable.ColorDrawable
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.PopupWindow
 
 class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+        val popupButton = findViewById<Button>(R.id.retos)
+        val popupView = LayoutInflater.from(this).inflate(R.layout.activity_popup, null)
+        val popupWindow = PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+
+        popupWindow.setBackgroundDrawable(ColorDrawable(android.graphics.Color.BLACK))
+        popupWindow.isFocusable = true
+        popupWindow.isOutsideTouchable = true
+
+        popupButton.setOnClickListener {
+            popupWindow.showAtLocation(popupButton, Gravity.BOTTOM, 0, 600)
+        }
+
+        /*val closeButton = popupView.findViewById<Button>(R.id.buttonClosePopup)
+        closeButton.setOnClickListener {
+            popupWindow.dismiss()
+        }*/
+
+        popupView.setOnTouchListener { _ , _ ->
+            if (popupWindow.isShowing) {
+                popupWindow.dismiss()
+            }
+            true
+        }
     }
 }
