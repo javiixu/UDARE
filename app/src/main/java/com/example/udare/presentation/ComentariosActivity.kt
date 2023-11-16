44c2aed325a8b0b6acba5f95ef67b464e2f79021
javi <javi.polit@hotmail.com>
2023-11-16 20:07:57 +0100

    Comentarios mejorados

--- app/src/main/java/com/example/udare/presentation/ComentariosActivity.kt
+++ app/src/main/java/com/example/udare/presentation/ComentariosActivity.kt
@@ -1,7 +1,10 @@
 package com.example.udare.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Button
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.udare.Adapter.CommentsAdapter
@@ -9,9 +12,15 @@ import com.example.udare.Adapter.FotoAdapter
 import com.example.udare.R
 
 class ComentariosActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comentarios)
+
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+
+        val backButton = findViewById<Button>(R.id.bAtras)
 
         val Lista: MutableList<String> = mutableListOf("hola", "mundo")
 
@@ -24,5 +33,11 @@ class ComentariosActivity : AppCompatActivity() {
 
         // Establecer el adaptador en el RecyclerView
         recyclerView.adapter = textoAdapter
+
+        backButton.setOnClickListener(){
+            Intent(this, Inicio::class.java).also{
+                startActivity(it)
+            }
+        }
     }
 }
