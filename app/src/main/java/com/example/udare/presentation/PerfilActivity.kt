9ba14614e38f4fe7a7e692e76ad98bd1987d0b53
David Tzafrir <david.tzafrir@campus.tu-berlin.de>
2023-11-16 17:24:17 +0100

    User Name and Bio are presented in Profile now

--- app/src/main/java/com/example/udare/presentation/PerfilActivity.kt
+++ app/src/main/java/com/example/udare/presentation/PerfilActivity.kt
@@ -2,6 +2,7 @@ package com.example.udare.presentation
 
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
+import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -13,11 +14,18 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import com.bumptech.glide.Glide
 import com.example.udare.R
+import com.example.udare.data.model.Post
 import com.example.udare.data.model.User
+import com.example.udare.data.repositories.Implementations.PostRepository
 import com.example.udare.data.repositories.Implementations.UserRepository
 import com.example.udare.services.interfaces.IUserService
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
+import java.io.FileOutputStream
+import java.util.Date
+import java.util.Locale
 import javax.inject.Inject
 
 
@@ -33,22 +41,22 @@ class PerfilActivity : AppCompatActivity() {
         setContentView(R.layout.activity_perfil)
 
         //Buttons & Views
-        var btnChangeProfilePicture: Button
-        var ivProfilePicture : ImageView
-        val tvSocialPoints = findViewById<TextView>(R.id.tvSocialPoints)
-        val tvSportPoints = findViewById<TextView>(R.id.tvSportPoints)
-        val tvGrowthPoints = findViewById<TextView>(R.id.tvGrowthPoints)
-        val tvCulturePoints = findViewById<TextView>(R.id.tvCulturePoints)
-        val tvCookingPoints = findViewById<TextView>(R.id.tvCookingPoints)
+        var tvSocialPoints = findViewById<TextView>(R.id.tvSocialPoints)
+        var tvSportPoints = findViewById<TextView>(R.id.tvSportPoints)
+        var tvGrowthPoints = findViewById<TextView>(R.id.tvGrowthPoints)
+        var tvCulturePoints = findViewById<TextView>(R.id.tvCulturePoints)
+        var tvCookingPoints = findViewById<TextView>(R.id.tvCookingPoints)
         val btnUserAlbum = findViewById<Button>(R.id.btnUserAlbum)
         val btnRankingFriends = findViewById<Button>(R.id.btnRankingFriends)
+        val btnChangeProfilePicture = findViewById<Button>(R.id.btnChangeProfilePicture)
+        var ivProfilePicture = findViewById<ImageView>(R.id.profilePicture)
+        var tvUserName = findViewById<TextView>(R.id.tvUserName)
+        var tvUserBio = findViewById<TextView>(R.id.tvUserBio)
 
         //TODO
-        //check if user has profile picture
-        //if not set default profile picture
         // if user updates profile pic send it to database
 
-        //get the data of the user, who is logged in and display his points
+        //get the data of the user, who is logged in and display his points, profile pic, username and bio
         userService.getUserById(THIS_USER_ID, object : UserRepository.callbackGetUserById {
             override fun onSuccess(user: User) {
                 thisUser = user
@@ -63,8 +71,14 @@ class PerfilActivity : AppCompatActivity() {
                 //otherwise we have to set it
                 if(thisUser.profile.profilePic != "Unspecified"){
                     Log.d("tag-prueba",thisUser.profile.profilePic)
-                    ivProfilePicture =  (thisUser.profile.profilePic) as ImageView
+                    Glide.with(this@PerfilActivity).load(thisUser.profile.profilePic).into(ivProfilePicture)
+
                 }
+
+                //set Bio and Username
+                tvUserBio.text = thisUser.profile.bio.toString()
+                tvUserName.text = thisUser.username.toString()
+
 
 
             }
@@ -83,12 +97,6 @@ class PerfilActivity : AppCompatActivity() {
 
         var imageUri : Uri
 
-        // initializing Profile Picture and Change profile Button
-        btnChangeProfilePicture = findViewById(R.id.btnChangeProfilePicture)
-        ivProfilePicture = findViewById(R.id.profilePicture)
-
-
-
 
         // Registers a photo picker activity launcher in single-select mode.
         val pickMedia =
@@ -101,7 +109,6 @@ class PerfilActivity : AppCompatActivity() {
 
                     // set the profile Picture to the selected uri
                     //TODO send this profile picture to the database
-                    //check if there is a profile picture set in the database in the start of the activity
                     val bitmap:Bitmap = getCapturedImage(imageUri)
                     ivProfilePicture.setImageBitmap(bitmap)
 
@@ -137,6 +144,36 @@ class PerfilActivity : AppCompatActivity() {
             }
         }
         return bitmap
+    }
+
+    fun uploadProfilePicture(bitmap: Bitmap) {
+        try {
+            val imageName = java.text.SimpleDateFormat("dd_MM_yyyy_HH_mm_ss", Locale("es", "ES"))
+                .format(Date(System.currentTimeMillis())) + "_perfil"
+            val file = File(this.applicationContext.filesDir, imageName)
+            val fileOutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+            fileOutputStream.flush()
+            fileOutputStream.close()
+
+            //this can't work we need some way to create the aws link here
+            //thisUser.profile.profilePic = file
+            /*
+            userService.updateUser(THIS_USER_ID, thisUser, object : UserRepository.callbackUpdateUser{
+                override fun onSuccess(user: User?) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onError(mensajeError: String?) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+
+             */
+        } catch (e: Exception) {
+            Log.e("tag-foto", "Error al subir la foto: ${e.message}")
+        }
     }
 
 }
