f8f35d529d9284a5999e07a759a5b1774e43ac92
javi <javi.polit@hotmail.com>
2023-11-17 00:23:50 +0100

    Comentarios con foto de perfil

--- app/src/main/java/com/example/udare/presentation/ComentariosActivity.kt
+++ app/src/main/java/com/example/udare/presentation/ComentariosActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.udare.Adapter.CommentsAdapter
@@ -18,9 +19,10 @@ class ComentariosActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_comentarios)
 
-        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
 
         val backButton = findViewById<Button>(R.id.bAtras)
+        val sendButton = findViewById<Button>(R.id.bEnviarComentario)
+        val comentarioListener = findViewById<EditText>(R.id.listenerComentario)
 
         val Lista: MutableList<String> = mutableListOf("hola", "mundo")
 
@@ -38,6 +40,11 @@ class ComentariosActivity : AppCompatActivity() {
             Intent(this, Inicio::class.java).also{
                 startActivity(it)
             }
+        }
+
+        sendButton.setOnClickListener {
+            val comentario = comentarioListener.text.toString()
+            comentarioListener.setText("")
         }
     }
 }
