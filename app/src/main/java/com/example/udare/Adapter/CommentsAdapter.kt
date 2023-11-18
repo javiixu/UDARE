45cfe6aff0cd5a44e248ed89fb125d2f8e26afc2
javi <javi.polit@hotmail.com>
2023-11-18 04:08:03 +0100

    Comentarios funcionando correctamente

--- app/src/main/java/com/example/udare/Adapter/CommentsAdapter.kt
+++ app/src/main/java/com/example/udare/Adapter/CommentsAdapter.kt
@@ -3,6 +3,7 @@ package com.example.udare.Adapter
 
 import android.content.Context
 import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,14 +13,17 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.udare.R
+import com.example.udare.data.model.CommentData
 import com.example.udare.presentation.Inicio
 
 
-class CommentsAdapter(private val textos: List<String>, private val context: Context) :
+class CommentsAdapter(private val Lista: List<CommentData>, private val context: Context) :
     RecyclerView.Adapter<CommentsAdapter.TextoHolder>(){
 
     class TextoHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextView = itemView.findViewById(R.id.comentario)
+        val commentView: TextView = itemView.findViewById(R.id.comentario)
+        val usernameView: TextView = itemView.findViewById(R.id.username)
+        val profilePicView: ImageView = itemView.findViewById(R.id.avatar)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextoHolder {
@@ -28,15 +32,25 @@ class CommentsAdapter(private val textos: List<String>, private val context: Con
     }
 
     override fun getItemCount(): Int {
-        return textos.size
+        return Lista.size
     }
 
     override fun onBindViewHolder(holder: TextoHolder, position: Int) {
-        val texto = textos[position]
+
+        val elem = Lista[position]
+        val comment = elem.comment
+        val profilePic = elem.profilePic
+        val username = elem.username
+
+        Log.d("tag-comments", username + comment + profilePic)
 
         // Configurar el texto en el TextView
-        holder.textView.text = texto
-
+        holder.commentView.text = comment
+        holder.usernameView.text = username
+        Glide.with(holder.profilePicView)
+            .load(profilePic) // Asegúrate de que CommentData tenga un campo profilePic
+            .apply(RequestOptions.circleCropTransform())
+            .into(holder.profilePicView)
     }
 
 
